f1a1b28eba0948d34db72d93c2dc16f56fc38758
Lidor Shimon <lidorsh2000@gmail.com>
2024-12-14 22:24:14 +0200

    Handling some events

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Hazel/Events/ApplicationEvent.h"
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
@@ -11,14 +12,15 @@ namespace Hazel {
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
-
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 	public:
 		Application();
 		virtual ~Application();
 
 		void Run();
 
-
+		void OnEvent(Event& e);
 	};
 
 	// To be defined in client
